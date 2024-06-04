85c8745029419febcb181c9dfa58a43a21381384
Oleg <kogolenoko@gmail.com>
2024-06-04 20:28:32 +0300

    Возведение в квадрат

---
+++ seminars/sem2/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+//методы-функции
+int Pow (int n)
+{
+    return n*n;
+} 
+System.Console.WriteLine("Input number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(Pow(a));//если здесь убрать System.Console.WriteLine, а оставить просто Pow(a), выведет просто введённое число 
