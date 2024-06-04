187241358640ffffaa7cb6b74b5d904e6cfe93db
Oleg <kogolenoko@gmail.com>
2024-06-04 22:21:08 +0300

    Не запускается

--- seminars/sem2/Program.cs
+++ seminars/sem2/Program.cs
@@ -17,17 +17,53 @@
 
 //Но функция может и не возвращать данные, применяется функция void
 // Использование void
-int Pow (int n)
-{
-    return n*n;
-} 
-void Pow2(int n)
+// int Pow (int n)
+// {
+//     return n*n;
+// } 
+// void Pow2(int n)
 
+// {
+//     System.Console.WriteLine($"Quard of {n} -> {n*n}");
+// }
+
+// System.Console.WriteLine("Input number: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+
+// Pow2(a);
+
+//                  TASK1
+
+//написать программу где вводится 3-х значное число и удаляется 2-я цифра этого числа Н.р. 256 - 26, 891 - 84
+
+
+//Сначала нужно определить 3-х значное число или нет
+
+bool IsThreeDigit(int num)//bool либо да либо нет
 {
-    System.Console.WriteLine($"Quard of {n} -> {n*n}");
+    if (num > 99 $$ num < 1000)
+    {
+        return true;
+    }
+    return false;
+}
+
+
+int DeleteSecondDigit(int num)//теперь напишем функцию, которая удалит 2-ю цифру 3-х значного числа
+{
+    if (IsThreeDigit(num))
+    {
+        int ed = num % 10;
+        int sot = num / 100;
+        return ed + sot * 10;
+    }
+    else
+    {
+        System.Console.WriteLine("You input no Three-digit number");
+        return 0;//если число не 3-х значное выводить что то всё равно должен Н.р. n
+    }
 }
 
 System.Console.WriteLine("Input number: ");
 int a = Convert.ToInt32(Console.ReadLine());
-
-Pow2(a);
+System.Console.WriteLine(DeleteSecondDigit(a));
