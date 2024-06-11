9464e39e479723f6873a1d02b9ebb45108f4252c
Oleg <kogolenoko@gmail.com>
2024-06-11 22:02:24 +0300

    sem3/2

--- seminars/sem3/Program.cs
+++ seminars/sem3/Program.cs
@@ -9,35 +9,35 @@
 //int [] - говорит, что возвращаться будет массив, а не число
 // дальше назовём его (как угодн)
 //н.р. CreateRandomArray()
-int[] CreateRandomArray(int size, int min, int max)
-{
-int[] array = new int[size];
-Random random = new Random();
-for(int i = 0; i < array.Length; i++)
-{
-    array[i] = random.Next(min, max + 1);
-//     //max+1-чтобы последняя цифра массива включительно
-}
-return array;
-}
-void PrintArray(int[] array)  //он выведет и закончит работу
-{
-for(int i = 0; i < array.Length; i++)
-{
-System.Console.Write(array[i] + " ");  //Просто Write
-}
-System.Console.WriteLine();
-}
+// int[] CreateRandomArray(int size, int min, int max)
+// {
+// int[] array = new int[size];
+// Random random = new Random();
+// for(int i = 0; i < array.Length; i++)
+// {
+//     array[i] = random.Next(min, max + 1);
+// //     //max+1-чтобы последняя цифра массива включительно
+// }
+// return array;
+// }
+// void PrintArray(int[] array)  //он выведет и закончит работу
+// {
+// for(int i = 0; i < array.Length; i++)
+// {
+// System.Console.Write(array[i] + " ");  //Просто Write
+// }
+// System.Console.WriteLine();
+// }
 
-System.Console.WriteLine("Input size array ");
-int size = Convert.ToInt32(Console.ReadLine());
-System.Console.WriteLine("Input min ");
-int min = Convert.ToInt32(Console.ReadLine());
-System.Console.WriteLine("Input max ");
-int max = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Input size array ");
+// int size = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Input min ");
+// int min = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Input max ");
+// int max = Convert.ToInt32(Console.ReadLine());
 
-int[] myArray = CreateRandomArray(size, min, max);
-//System.Console.WriteLine(myArray); это метод не предназначено для вывода массива
-//нужно писать свой метод для вывода массива с 23 строки
-PrintArray(myArray);
+// int[] myArray = CreateRandomArray(size, min, max);
+// //System.Console.WriteLine(myArray); это метод не предназначено для вывода массива
+// //нужно писать свой метод для вывода массива с 23 строки
+// PrintArray(myArray);
 
