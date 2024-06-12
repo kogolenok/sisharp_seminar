64f79232ba990688431cebd9e48ccbafc9cb89b8
Oleg <kogolenoko@gmail.com>
2024-06-12 21:41:22 +0300

    task1

--- seminars/sem3/Program.cs
+++ seminars/sem3/Program.cs
@@ -1,43 +1,62 @@
-﻿//                 ARRAY
-//          TASK1
+﻿ //               ARRAY
+  //       TASK1
 // задать массив
-//определить есть ли число 
-//в этом массиве выводить Да или Нет
-//н.р. (1,2,3,5) - 6 - нет
-//н.р. (1,2,3,4,5) 2 - да
+// определить есть ли число 
+// в этом массиве выводить Да или Нет
+// н.р. (1,2,3,5) - 6 - нет
+// н.р. (1,2,3,4,5) 2 - да
 // метод задающий массив
 //int [] - говорит, что возвращаться будет массив, а не число
-// дальше назовём его (как угодн)
-//н.р. CreateRandomArray()
-// int[] CreateRandomArray(int size, int min, int max)
-// {
-// int[] array = new int[size];
-// Random random = new Random();
-// for(int i = 0; i < array.Length; i++)
-// {
-//     array[i] = random.Next(min, max + 1);
-// //     //max+1-чтобы последняя цифра массива включительно
-// }
-// return array;
-// }
-// void PrintArray(int[] array)  //он выведет и закончит работу
-// {
-// for(int i = 0; i < array.Length; i++)
-// {
-// System.Console.Write(array[i] + " ");  //Просто Write
-// }
-// System.Console.WriteLine();
-// }
+// дальше назовём его (как угодно)
+// н.р. CreateRandomArray()
+int[] CreateRandomArray(int size, int min, int max)
+{
+int[] array = new int[size];
+Random random = new Random();
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = random.Next(min, max + 1);
+//max+1-чтобы последняя цифра массива включительно
+}
+return array;
+}
+void PrintArray(int[] array)  //он выведет и закончит работу
+{
+for(int i = 0; i < array.Length; i++)
+{
+System.Console.Write(array[i] + " ");  //Просто Write
+}
+System.Console.WriteLine();
+}
+//сделаю метод, который будет определять есть ли число в массиве
+bool SearchNum(int[] array, int num)
+{
+   for(int i = 0; i < array.Length; i++)
+   {
+    if(array [i] == num)
+    {
+        return true;
+    }
+   } 
+   return false;
+}
 
-// System.Console.WriteLine("Input size array ");
-// int size = Convert.ToInt32(Console.ReadLine());
-// System.Console.WriteLine("Input min ");
-// int min = Convert.ToInt32(Console.ReadLine());
-// System.Console.WriteLine("Input max ");
-// int max = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Input size array ");
+int size = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Input min ");
+int min = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Input max ");
+int max = Convert.ToInt32(Console.ReadLine());
 
-// int[] myArray = CreateRandomArray(size, min, max);
-// //System.Console.WriteLine(myArray); это метод не предназначено для вывода массива
-// //нужно писать свой метод для вывода массива с 23 строки
-// PrintArray(myArray);
+
+int[] myArray = CreateRandomArray(size, min, max);
+//System.Console.WriteLine(myArray); это метод не предназначено для вывода массива
+//нужно писать свой метод для вывода массива с 23 строки
+PrintArray(myArray);
+
+System.Console.WriteLine("Input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(SearchNum(myArray, num));
+
+
 
