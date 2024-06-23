265853ff100fcf562b6809b8aa4f2c1c79321389
Oleg <kogolenoko@gmail.com>
2024-06-23 19:56:56 +0300

    Lesson 4 Task 1,2

--- seminars/sem4/Program.cs
+++ seminars/sem4/Program.cs
@@ -22,3 +22,46 @@
 // System.Console.WriteLine("Input number: ");
 // int num = Convert.ToInt32(Console.ReadLine());
 // System.Console.WriteLine(DigitsCounter(num));
+
+//         TASK2
+//Задать массив случайных чисел N, 
+//который вводится с клавиатуры
+//найти кол-во чисел одновременно оканчивающихся
+//на 1 и делящихся на цело на 7
+//н.р. (1 5 11 21 91)
+//ответ 2 
+
+              //РЕШЕНИЕ
+//num = num +(*-) 1; => num +(*-)=
+
+//int[] InputArray(int size)//это метод создающий массив
+//здесь создаём и заполняем массив, и возвращаем
+// {
+//     int[] arr = new int[size];// массив с размером size
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         System.Console.WriteLine($"Input {i + 1} number: ");
+//         arr[i] = Convert.ToInt32(Console.ReadLine());
+//     }
+//     return arr;
+// }
+// //пишем метод, который и будет считать кол-во элементов массива
+// int ArrayCounter(int[] arr)
+// {
+//     int count = 0;
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         if (arr[i] % 10 == 1 && arr[i] % 7 == 0)
+//         {
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+// //запрошу размер массива
+// Console.WriteLine("Input array size: ");
+// int size = Convert.ToInt32(Console.ReadLine());
+// //теперь вызову метод для ввода массива
+// int[] arr = InputArray(size);
+// //теперь когда есть массив найдём наши числа
+// System.Console.WriteLine(ArrayCounter(arr));
