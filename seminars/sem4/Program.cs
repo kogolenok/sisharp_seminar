056653ce8814fd7c870549b825ffab0d7850f6d7
Oleg <kogolenoko@gmail.com>
2024-06-23 21:52:54 +0300

    Lesson 4 1,2,3

--- seminars/sem4/Program.cs
+++ seminars/sem4/Program.cs
@@ -34,8 +34,8 @@
               //РЕШЕНИЕ
 //num = num +(*-) 1; => num +(*-)=
 
-//int[] InputArray(int size)//это метод создающий массив
-//здесь создаём и заполняем массив, и возвращаем
+// int[] InputArray(int size)//это метод создающий массив
+// //здесь создаём и заполняем массив, и возвращаем
 // {
 //     int[] arr = new int[size];// массив с размером size
 //     for (int i = 0; i < arr.Length; i++)
@@ -65,3 +65,51 @@
 // int[] arr = InputArray(size);
 // //теперь когда есть массив найдём наши числа
 // System.Console.WriteLine(ArrayCounter(arr));
+
+               //TASK 3
+//Ввести с консоли массив случайных чисел
+//от 0 до 9 и записать эти цифры как одно число
+//старший разряд числа находится на 0-м индексе
+//младший на - на последнем
+//н.р. 3 2 1 - 321
+//1 3 2 4 - 1324
+
+               //РЕШЕНИЕ
+
+// int[] CreateRandomArray(int size)
+// {
+//     int[] array = new int[size];
+//     Random random = new Random();
+//     for(int i = 0; i < array.Length; i++)
+//     {
+//         array [i] = random.Next(0, 9 + 1);
+//     }
+//     return array;
+// }
+
+// void PrintArray (int[] array)
+// {
+//   for(int i = 0; i < array.Length; i++)
+//   {
+//     System.Console.Write(array[i] + " ");//здесь если WriteLine
+//     //если выше WriteLine написать то он в столбец будет выводить
+//   }  
+//   System.Console.WriteLine();
+// }
+
+// int NumberOfArray (int[] array)
+// {
+//     int number = 0;
+//     for(int i = 0; i < array.Length; i++)
+//     {
+//         number += array[i] * (int)Math.Pow(10, (array.Length - i-1));
+//     }
+//     return number;
+// }
+// Console.WriteLine("Input array size: ");
+// int size = Convert.ToInt32(Console.ReadLine());
+
+// int[] myArray = CreateRandomArray(size);
+// int result = NumberOfArray(myArray);
+// PrintArray(myArray);
+// System.Console.WriteLine(result);
