b0e98ce3741e691b7f03294358f219cb3a8e5316
Oleg <kogolenoko@gmail.com>
2024-06-23 19:04:33 +0300

    Lesson 4/1

---
+++ seminars/sem4/Program.cs
@@ -0,0 +1,24 @@
+﻿//                Lesson 4 ФУНУЦИИ
+
+//          Task 1
+// Написать программу, которая на вход
+//принимает число, а на выходе - кол-во
+// цифр в числе
+//  н.р. 458 - 3
+//22 - 2
+// 1-1
+// РЕШЕНИЕ
+// int DigitsCounter(int num)
+// {
+//     int counter = 0;
+//     while (num > 0)
+//     {
+//         num /= 10;
+//         counter++;
+//     }
+//     return counter;
+// }
+
+// System.Console.WriteLine("Input number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine(DigitsCounter(num));
